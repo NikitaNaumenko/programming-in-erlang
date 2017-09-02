0029ed84c07768ea34ffdbd5fc030d2d5a3044cf
Naumenko Nikita <alerto_93@mail.ru>
2017-09-02 17:19:13 +0300

    complete 3.6

---
+++ chapter3/3.6_sort_lists.erl
@@ -0,0 +1,27 @@
+- module(3.6_sort_lists).
+- export([fast_sort/1,  merge_sort/1]).
+- export([test/0]).
+
+quicksort([]) -> [];
+quicksort([Head | Tail]) ->
+  [Less| Greater] -> split([Head, Tail, [],[]]),
+  quicksort(Less) ++ [Head] ++ quicksort(Greater)
+
+split(Head,[F|T], Less, Greater) when TailHead < Head -> split(Head, Tail,[F|T], Greater);
+split(Head,[F|T], Less, Greater) -> split(Head, Tail, Less, [F|T]);
+split(_, [], Less, Greater) -> [L, H]
+
+
+merge_sort([]) -> [];
+merge_sort(R)->
+  [R] = merge([[X]||X<-L]),
+  R.
+
+merge([])-> [];
+merge([A, B|T]) -> merger([merge(A, B)| merge(T)]);
+merge([A]) -> [A].
+
+merge([A|TA], [B|_] = LB) when A<B -> [A|merge(TA, LB)];
+merge([_|_] = LA, [B|TB]) -> [B|merge(LA, TB)];
+merge([], LB) -> LB;
+merge(LA, []) -> LA.
