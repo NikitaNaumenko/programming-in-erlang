9efc1aa66c04bdca7d59646422db5463aa844c78
Naumenko Nikita <alerto_93@mail.ru>
2017-09-10 23:11:32 +0300

    complete lexer

--- chapter3/simple_compiler.erl
+++ chapter3/simple_compiler.erl
@@ -1,45 +1,47 @@
 - module(simple_compiler).
 
-- export([evualator/1, printer/1, compiler/1, simulator/1, converter/1]).
+-compile(export_all).
 
-- export([test/0]).
+%- export([evualator/1, printer/1, compiler/1, simulator/1, converter/1]).
+
+%- export([test/0]).
 
 parser(Expression) -> parser(lexer(Expression)).
 
-lexer('if'++Record) -> ['if' | lexer(Record)];
-lexer('then'++Record) -> ['then' | lexer(Record)];
-lexer('else'++Record) -> ['else' | lexer(Record)];
-lexer('let'++Record) -> ['let' | lexer(Record)];
-lexer('in'++Record) -> ['in' | lexer(Record)];
-lexer([$= | Record]) -> ['=' | lexer(Record)];
-lexer([$( | Record]) -> ['(' | lexer(Record)];
-lexer([$) | Record]) -> [')' | lexer(Record)];
-lexer([$~ | Record]) -> ['~' | lexer(Record)];
-lexer([$+ | Record]) -> ['+' | lexer(Record)];
-lexer([$- | Record]) -> ['-' | lexer(Record)];
-lexer([$* | Record]) -> ['*' | lexer(Record)];
-lexer([$/ | Record]) -> ['/' | lexer(Record)];
-lexer([$\s| Record]) -> lexer(Record);
-lexer([Xpression | _] = List) -> when Xpression <= $9, Xpression =>$0 ->
-  { Num, Record } = lex_num(List, 0),
-  [{ num, Num } | lexer(R)];
-lexer([Xpression | Record]) when X =< $z, X >= $a; X =< $Z, X >= $A ->
-  { I, R2 } = lex_id(R, [_ | X]),
-  [{ id, I } | lexer(Record2)]l
+
+lexer("if"++R) -> ['if'|lexer(R)];
+lexer("then"++R) -> ['then'|lexer(R)];
+lexer("else"++R) -> ['else'|lexer(R)];
+lexer("let"++R) -> ['let'|lexer(R)];
+lexer("in"++R) -> ['in'|lexer(R)];
+lexer([$=|R]) -> ['='|lexer(R)];
+lexer([$(|R]) -> ['('|lexer(R)];
+lexer([$)|R]) -> [')'|lexer(R)];
+lexer([$~|R]) -> ['~'|lexer(R)];
+lexer([$+|R]) -> ['+'|lexer(R)];
+lexer([$-|R]) -> ['-'|lexer(R)];
+lexer([$*|R]) -> ['*'|lexer(R)];
+lexer([$/|R]) -> ['/'|lexer(R)];
+lexer([$\s|R]) -> lexer(R);
+lexer([X|_] = L) when X =< $9, X >= $0 ->
+  {Num, R} = lex_num(L, 0),
+  [{num, Num}|lexer(R)];
+lexer([X|R]) when X =< $z, X >= $a; X =< $Z, X >= $A ->
+  {I, R2} = lex_id(R, [X]),
+  [{id, I}|lexer(R2)];
 lexer([]) -> [].
 
+lex_num([X|R], N) when X =< $9, X >= $0 ->
+  lex_num(R, 10*N + X - $0);
+lex_num([$.|R], N) ->
+  {F, R2} = lex_fract(R, 0, 0.1),
+  {N + F, R2};
+lex_num(R, N) -> {N, R}.
 
-lex_num([Xpression | Record], Num) when Xpression <= $9, Xpression => $0 ->
-  lex_num(Record, 10 * Num + Xpression - $0);
-lex_num([$. | Record], Num) ->
-    {Fract, Record2} = lex_fract(Record, 0, 0, 1),
-    {Num + Fract, Record2};
-lex_num(Record, Num) -> {Num, Record}.
-
-lex_fract([Xpression | Record], Num, Fract) when Xpression <= $9, Xpression => $0 ->
-  lex_fract(Record, Num + Fract * (Xpression - $0), Fract/10);
-lex_fract(Record, Num, _) -> {Num ,Record}.
+lex_fract([X|R], N, F) when X =< $9, X >= $0 ->
+  lex_fract(R, N + F * (X - $0), F/10);
+lex_fract(R, N, _) -> {N, R}.
 
 lex_id([X|R], I) when X =< $z, X >= $a; X =< $Z, X >= $A; X =< $9, X >= $0 ->
-  lex_id(R, [X | I]);
+  lex_id(R, [X|I]);
 lex_id(R, I) -> {lists:reverse(I), R}.
