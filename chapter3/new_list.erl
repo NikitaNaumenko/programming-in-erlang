b8a0552b5da986f48f3b9e5f3697f1df903f5f6c
Naumenko Nikita <naumenkoniki@gmail.com>
2019-03-01 21:20:30 +0300

    Add 3.2

---
+++ chapter3/new_list.erl
@@ -0,0 +1,17 @@
+-module(new_list).
+-export([create/1, reverse_create/1, test/0]).
+
+create(N) -> create(N, []).
+create(0, Acc) -> Acc;
+create(N, Acc) -> create(N - 1, [N | Acc]).
+
+reverse_create(N) -> reverse_create(1, N, []).
+reverse_create(N, N, Acc) -> [N | Acc];
+reverse_create(M, N, Acc) -> reverse_create(M + 1, N, [M | Acc]).
+
+test() ->
+  [1, 2, 3] = create(3),
+  [1, 2, 3, 4] = create(4),
+  [3, 2, 1] = reverse_create(3),
+  [4, 3, 2, 1] = reverse_create(4),
+  ok.
